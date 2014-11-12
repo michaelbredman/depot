0a13d2d4cdf4d3b41bb5bd2479a74724f759f5c2
michaelbredman <mredman@gmail.com>
2014-11-12 15:29:17 -0800

    testing Se

--- test/selenium/basic.rb
+++ test/selenium/basic.rb
@@ -1,25 +1,9 @@
 require 'selenium-webdriver'
-#require 'rspec-expectaions'
-#caps = Selenium::WebDriver::Remote::Capabilities.new
-#caps["browserName"] = "chrome"
-#caps["verison"] = ""
-#caps["platform"] = "Windows 8.1"
-#caps["name"] = "Travis build: #{ENV['TRAVIS_JOB_NUMBER']}"
-#caps["tunnel-identifier"] = "#{ENV['TRAVIS_JOB_NUMBER']}"
-
-# In order to conenct to Sauce Labs you will need your Sauce Labs username and access key.
-# Your username and access key can be found here https://saucelabs.com/account
-#driver = Selenium::WebDriver.for(:remote,
-#:url => "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub",
-#:desired_capabilities => caps)
-#driver.navigate.to "http://localhost:3000/products"
-#driver.navigate.to "http://www.saucelabs.com"
-#driver.quit
-
 
 BROWSERS = { firefox: '27',
 			chrome: '32',
-			internet_explorer: '8'}
+			internet_explorer: '8',
+			chrome: '33'}
 
 def setup(browser_name, browser_version)
 	caps = Selenium::WebDriver::Remote::Capabilities.send(browser_name.to_sym)
@@ -29,8 +13,7 @@ def setup(browser_name, browser_version)
 	caps["build"] = "#{ENV['TRAVIS_JOB_NUMBER']}"
 	caps["tunnel-identifier"] = "#{ENV['TRAVIS_JOB_NUMBER']}"
 
-	Thread.current[:driver] = Selenium::WebDriver.for(
-		:remote,
+	Thread.current[:driver] = Selenium::WebDriver.for(:remote,
 		url: "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub",
 		desired_capabilities: caps)
 end
@@ -52,5 +35,5 @@ def teardown
 end
 
 run do
-	Thread.current[:driver].get 'http://www.saucelabs.com'
+	Thread.current[:driver].get 'http://localhost:5000'
 end
